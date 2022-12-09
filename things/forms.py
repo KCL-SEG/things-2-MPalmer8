0a050ee765555b5a9ae0faf7e2ceb815df97a21a
Matthew Palmer <matthewpalmer135@gmail.com>
2022-12-09 10:58:20 +0000

    Implement form

--- things/forms.py
+++ things/forms.py
@@ -1,10 +1,11 @@
 """Forms of the project."""
 
 from django import forms
+from .models import Thing
 
 class ThingsForm(forms.ModelForm):
   class Meta:
     model = Thing
     fields = ['name',]
-    description = forms.Textarea(max_length=120, blank=True)
+    description = forms.Textarea()
     quantity = forms.NumberInput()
--- things/views.py
+++ things/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import ThingsForm
 
 def home(request):
+    return render(request, 'home.html', {'form': form})
+
+def things(request):
+    form = ThingsForm()
     return render(request, 'home.html', {'form': form})
