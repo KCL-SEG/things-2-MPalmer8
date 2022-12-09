ba0978c3587d96a1925455fecdb8ed14567c1a44
Matthew Palmer <matthewpalmer135@gmail.com>
2022-12-09 10:59:55 +0000

    Fix naming error

--- things/views.py
+++ things/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .forms import ThingsForm
+from .forms import ThingForm
 
 def home(request):
     return render(request, 'home.html', {'form': form})
 
 def things(request):
-    form = ThingsForm()
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
